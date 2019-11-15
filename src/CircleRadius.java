618a92e260f70beb22c40fb91633e3a23d2f468a
leviminch04 <54951562+leviminch04@users.noreply.github.com>
2019-11-14 17:23:46 -0800

    Finished CircleRadius besides one bug

--- src/CircleRadius.java
+++ src/CircleRadius.java
@@ -1,14 +1,16 @@
+import com.sun.prism.paint.Color;
 import javafx.event.ActionEvent;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.shape.Circle;
 
 
-public class CircleRadius extends GridPane {
+public class CircleRadius extends GridPane{
     private TextField radius;
     private String radiusCircleString;
     private double radiusCircle;
@@ -20,20 +22,23 @@ public class CircleRadius extends GridPane {
         Font font = new Font(20);
         radius = new TextField();
         radius.setFont(font);
-        radius.setPrefWidth(70);
+        radius.setPrefWidth(25);
         radius.setAlignment(Pos.CENTER);
         radius.setOnAction(this::processReturn);
 
-        circle.setCenterX(100f);
-        circle.setCenterY(100f);
-        circle.setRadius(0f);
+        circle.setCenterX(150f);
+        circle.setCenterY(150f);
+        circle.setRadius(50f);
+        circle.setStroke(Paint.valueOf("BLACK"));
+        circle.setFill(null);
 
         setAlignment(Pos.CENTER);
         setHgap(20);
         setVgap(10);
-        setStyle("-fx-background-color: LIGHTSTEELBLUE");
+        setStyle("-fx-background-color: WHITE");
 
-        add(radius, 1, 0);
+        add(radius, 0, 0);
+        add(circle, 0, 0);
     }
 
     public void processReturn(ActionEvent event)
